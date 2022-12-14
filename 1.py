2caa86484ce4aba1867f6b94fa7b220fb8386663
Vaishnavi Barge <89184566+25Vaishnavibarge17@users.noreply.github.com>
2022-12-15 02:23:49 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+# using for loop
 num=int(input("Enter a number: "))
 if num>1:
     for i in range(2,num):
@@ -10,3 +11,19 @@ if num>1:
 else:
     print(num," is not a prime number.")
             
+        
+# using while loop
+    
+"""num=int(input("Enter a number: "))
+i = 2
+flag = 0
+while i<num:
+    if num%i == 0:
+        flag = 1          #If Yes,update flag value
+        print (num,"is NOT a prime number!");
+        break
+    i = i + 1              #Updating the value of i on every iteration by 1
+#checking the value of flag
+if flag == 0:
+    print (num,"is a prime number!");      #If Yes, Then it is a prime number
+    """
